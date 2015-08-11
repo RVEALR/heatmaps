c3b68b70493fec8757346e9836f740d96eedc399
Marc Tanenbaum <marct@unity3d.com>
2015-08-11 10:14:29 -0700

    Removed try blocks from HeatmapDataParser. I want to see the errors, not bury them.

--- Heatmaps/Assets/Plugins/Heatmaps/HeatmapDataParser.cs
+++ Heatmaps/Assets/Plugins/Heatmaps/HeatmapDataParser.cs
@@ -28,20 +28,10 @@ namespace UnityAnalytics
 		}
 
 		protected void LoadStream(string path) {
-			// Handle any problems that might arise when reading the text
-			try
+			StreamReader reader = new StreamReader(path);
+			using (reader)
 			{
-				StreamReader reader = new StreamReader(path);
-				using (reader)
-				{
-					ConsumeHeatmapData (reader.ReadToEnd());
-				}
-			}
-			// If anything broke in the try block, we throw an exception with information
-			// on what didn't work
-			catch (Exception e)
-			{
-				Debug.Log(e.Message);
+				ConsumeHeatmapData (reader.ReadToEnd());
 			}
 		}
 
@@ -57,55 +47,50 @@ namespace UnityAnalytics
 			float maxDensity = 0;
 			float maxTime = 0;
 
-			try
+			Dictionary<string, object> data = Json.Deserialize(text) as Dictionary<string, object>;
+			foreach(KeyValuePair<string, object> kv in data)
 			{
-				Dictionary<string, object> data = Json.Deserialize(text) as Dictionary<string, object>;
-				foreach(KeyValuePair<string, object> kv in data)
+				keys.Add(kv.Key);
+
+				var pointList = kv.Value as List<object>;
+				HeatPoint[] array = new HeatPoint[pointList.Count];
+				for (int a = 0, aa = pointList.Count; a < aa; a++)
 				{
-					keys.Add(kv.Key);
+					array[a] = new HeatPoint();
+					float x = 0, y = 0, z = 0, t = 0;
+					float d = 0;
+					Dictionary<string, object> pt = pointList [a] as Dictionary<string, object>;
 
-					var pointList = kv.Value as List<object>;
-					HeatPoint[] array = new HeatPoint[pointList.Count];
-					for (int a = 0, aa = pointList.Count; a < aa; a++)
-					{
-						array[a] = new HeatPoint();
-						float x = 0, y = 0, z = 0, t = 0;
-						float d = 0;
-						Dictionary<string, object> pt = pointList [a] as Dictionary<string, object>;
-
-						foreach (KeyValuePair<string,object> pointKV in pt) {
-							switch (pointKV.Key)
-							{
-							case "x":
-								x = (float)(Double)pointKV.Value;
-								break;
-							case "y":
-								y = (float)(Double)pointKV.Value;
-								break;
-							case "z":
-								z = (float)(Double)pointKV.Value;
-								break;
-							case "t":
-								t = (float)(Double)pointKV.Value;
-								break;
-							case "d":
-								d = (float)(Int64)pointKV.Value;
-								break;
-							}
+					foreach (KeyValuePair<string,object> pointKV in pt) {
+						var value = (float)Convert.ToDouble (pointKV.Value);
+						switch (pointKV.Key)
+						{
+						case "x":
+							x = value;
+							break;
+						case "y":
+							y = value;
+							break;
+						case "z":
+							z = value;
+							break;
+						case "t":
+							t = value;
+							break;
+						case "d":
+							d = value;
+							break;
 						}
-						array[a].position = new Vector3(x,y,z);
-						array[a].density = d;
-						array [a].time = t;
-						maxDensity = Mathf.Max (d, maxDensity);
-						maxTime = Mathf.Max (array[a].time, maxTime);
 					}
-					heatData[kv.Key] = array;
+					array[a].position = new Vector3(x,y,z);
+					array[a].density = d;
+					array [a].time = t;
+					maxDensity = Mathf.Max (d, maxDensity);
+					maxTime = Mathf.Max (array[a].time, maxTime);
 				}
+				heatData[kv.Key] = array;
 			}
-			catch (Exception e)
-			{
-				Debug.LogError("JSON Parse error. " + e.Message);
-			}
+
 
 			if (handler != null) {
 				handler (heatData, maxDensity, maxTime, keys.ToArray (typeof(string)) as string[]);
