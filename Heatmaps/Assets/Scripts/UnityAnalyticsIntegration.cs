9407ca4982c0675a70495d825fa16e98850708a1
Marc Tanenbaum <marct@unity3d.com>
2016-04-30 09:25:51 -0700

    Added ifdef for Unity 4.7

--- Heatmaps/Assets/Scripts/UnityAnalyticsIntegration.cs
+++ Heatmaps/Assets/Scripts/UnityAnalyticsIntegration.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine;
 
-#if UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 ||  UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0
+#if UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 ||  UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_4_7 || UNITY_5_0
 using UnityEngine.Cloud.Analytics;
 
 #else
@@ -12,7 +12,7 @@ using UnityEngine.Analytics;
 public class UnityAnalyticsIntegration : MonoBehaviour
 {
 
-    #if UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 ||  UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0
+    #if UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 ||  UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_4_7 || UNITY_5_0
     void Start () {
         const string projectId = "Place your project ID here";
         UnityEngine.Cloud.Analytics.UnityAnalytics.StartSDK (projectId);
