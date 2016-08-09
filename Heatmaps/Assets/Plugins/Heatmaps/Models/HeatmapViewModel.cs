c67495b53869165a2e511055026f90e7205455a6
Marc Tanenbaum <marct@unity3d.com>
2016-08-09 08:54:10 -0700

    Created new models dir for plugin

---
+++ Heatmaps/Assets/Plugins/Heatmaps/Models/HeatmapViewModel.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// Heatmap view model.
+/// </summary>
+
+using System;
+
+namespace UnityAnalyticsHeatmap
+{
+    public class HeatmapViewModel
+    {
+        /// <summary>
+        /// The data directly represented in the map
+        /// </summary>
+        public HeatPoint[] m_HeatData;
+        /// <summary>
+        /// The raw data from which the map is generated.
+        /// </summary>
+        public CustomRawDatum[] m_RawData;
+    }
+}
+
