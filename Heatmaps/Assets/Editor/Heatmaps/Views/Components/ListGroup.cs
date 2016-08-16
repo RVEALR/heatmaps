5ee4813c5011a191ffc28fdddaff0f2584ef4ac5
Marc Tanenbaum <marct@unity3d.com>
2016-08-16 13:34:50 -0700

    New ListGroup component

---
+++ Heatmaps/Assets/Editor/Heatmaps/Views/Components/ListGroup.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// Visual component for managing a list of popup lists.
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityAnalyticsHeatmap
+{
+    public class AnalyticsListGroup
+    {
+
+        public delegate void ChangeHandler(List<int> value);
+
+
+        public static List<int> ListGroup(List<int>value, List<List<string>> lists, ChangeHandler change)
+        {
+            if (lists == null || value == null)
+            {
+                return value;
+            }
+            EditorGUI.BeginChangeCheck();
+            for(int a = 0; a < lists.Count; a++)
+            {
+                var listArray = lists[a].ToArray();
+                value[a] = EditorGUILayout.Popup(value[a], listArray);
+            }
+            if (EditorGUI.EndChangeCheck())
+            {
+                change(value);
+            }
+            return value;
+        }
+    }
+}
+
